d13e24fa663f9655a0f4952673c284fe6b5e6894
林劭全 <97021002+SHAO-QUAN-LIN@users.noreply.github.com>
2024-06-04 18:33:38 +0800

    Add files via upload

---
+++ Esay/1. Two Sum/1. Two Sum/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1.Two_Sum
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> Ans = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        Ans.Add(i);
+                        Ans.Add(j);
+
+                        break;
+                    }
+                }
+            }
+
+            foreach(int output in Ans)
+            {
+                Console.WriteLine(output);
+            }
+
+            //return Ans.ToArray();
+        }
+    }
+
+    public partial class MainWindow
+    {
+        int[] nums = { 2, 7, 11, 15 };
+
+        int target = 9;
+    }
+}
